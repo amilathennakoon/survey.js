479bb099b2047c644d523db9975732991320eb6b
Egbert Bouman <ebouman@gmail.com>
2015-04-13 11:16:08 +0200

    Various fixes + added single-select-online question type

--- js/survey.js
+++ js/survey.js
@@ -9,40 +9,143 @@ var questions = [
              id: '1', 
     break_after: true,
            type: 'text-field-large'},
-  {        text: 'What has a face and two hands but no arms or legs?', 
+  {        text: '<div><p><h2>Part 1 Demographic Questionnaire</h2></p><p><b>What is your age?</b></p></div>', 
              id: '2', 
-    break_after: true,
-       required: true,
-           type: 'single-select', 
-        options: ["a clock", "a dog", "a couch", "a giraffe"]},
-  {        text: 'What gets wetter and wetter the more it dries?', 
+           type: 'text-field-small'},
+  {        text: '<p><b>What is your gender?</b></p>', 
              id: '3', 
            type: 'single-select', 
-        options: ["a car", "a towel", "a plane", "a television"]},
-  {        text: 'You throw away the outside and cook the inside. Then you eat the outside and throw away the inside. What did you eat?', 
+        options: ["Female", "Male"]},
+  {        text: '<p><b>What is your nationality?</b></p>', 
              id: '4', 
-           type: 'single-select', 
-        options: ["a cucumber", "an ear of corn", "a watermelon", "cheese"]},
-  {        text: 'What goes up and down the stairs without moving?', 
+           type: 'text-field-small'},
+  {        text: '<p><b>How often do you watch online videos?</b></p>', 
              id: '5', 
            type: 'single-select', 
-        options: ["a toddler", "an arrow", "towels", "a rug"]},
-  {        text: 'What can you catch but not throw?', 
+        options: ["Everyday", "Several times a week", "Several times a month", "Several times a year", "Never"]},
+  {        text: '<p><b>What is the resolution of your screen?</b><br>Please write "DK" if you don\'t know the answer</p>', 
              id: '6', 
-           type: 'single-select', 
-        options: ["a couch", "a cold", "a puppy", "a baseball"]},
-  {        text: 'I can run but not walk. Wherever I go, thought follows close behind. What am I?', 
+           type: 'text-field-small'},
+  {        text: '<p><b>Are you interested in sports video?</b></p>', 
              id: '7', 
-           type: 'single-select', 
-        options: ["a doctor", "a pineapple", "a nose", "pimples"]},
-  {        text: 'What\'s black and white and red all over?', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Not at all", "Very much"]},
+  {        text: '<p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p>', 
              id: '8', 
-           type: 'single-select', 
-        options: ["an embarrased skunk", "a turtle", "a giraffe", "a dog"]},
-  {        text: 'What goes around the world but stays in a corner?', 
+    break_after: true,
+           type: 'text-field-large'},
+  {        text: '<div><p><h2>Part 2 Personality Questionnaire</h2><br>In the following part, there are a number of personality traits that may or may not apply to you. Please mark the extent to which you agree or disagree with that statement. You should rate the extent to which the pair of traits applies to you, even if one characteristic applies more strongly than the other.</p><p><h2>I see myself as</h2></p><p><b>Extraverted, enthusiastic</b></p></div>', 
              id: '9', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Critical, quarrelsome</b></p>', 
+             id: '10', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Dependable, self-disciplined</b></p>', 
+             id: '11', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Anxious, easily upset</b></p>', 
+             id: '12', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Open to new experiences, complex</b></p>', 
+             id: '13', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Reserved, quiet</b></p>', 
+             id: '14', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Sympathetic, warm</b></p>', 
+             id: '15', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Disorganized, careless</b></p>', 
+             id: '16', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Calm, emotionally stable</b></p>', 
+             id: '17', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>Conventional, uncreative</b></p>', 
+             id: '18', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["Disagree Strongly", "Agree Strongly"]},
+  {        text: '<p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p>', 
+             id: '19', 
+    break_after: true,
+           type: 'text-field-large'},
+   {       text: '<div><p><h2>Part 3 Training</h2><br>In this Part you will watch TWO sample videos, The first video represents the highest possible video quality you will get, the second video represents the lowest possible video quality you will get. The second video will not be played until you finished the first video. After watching these two videos, there is one sample question to help you get familiar with our test questionnaire.</p><p><h2>Sample High Quality</h2><iframe class="video" src="http://youtube.com/embed/2bOgv9vDp-A" style="width: 760px; height: 573px;"></iframe></p><p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p></div>', 
+             id: '20', 
+    break_after: true,
+           type: 'text-field-large'},
+   {       text: '<div><p><h2>Part 3 Training</h2><br>In this Part you will watch TWO sample videos, The first video represents the highest possible video quality you will get, the second video represents the lowest possible video quality you will get. The second video will not be played until you finished the first video. After watching these two videos, there is one sample question to help you get familiar with our test questionnaire.</p><p><h2>Sample Low Quality</h2><iframe class="video" src="http://youtube.com/embed/hnJxAlEdxmk" style="width: 760px; height: 573px;"></iframe></p><p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p></div>', 
+             id: '21', 
+    break_after: true,
+           type: 'text-field-large'},
+  {        text: '<div><p><h2>Part 3 Training</h2><br>In this Part you will watch TWO sample videos, The first video represents the highest possible video quality you will get, the second video represents the lowest possible video quality you will get. The second video will not be played until you finished the first video. After watching these two videos, there is one sample question to help you get familiar with our test questionnaire.</p><p><h2>Sample question</h2></p><p><b>The video clip was enjoyable</b></p></div>', 
+             id: '22', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["fully disagree", "fully agree"]},
+   {       text: '<p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p>', 
+             id: '23', 
+    break_after: true,
+           type: 'text-field-large'},
+   {       text: '<p><h2>The Training part is over. Please click continue to start the actual experiment</h2></p>', 
+             id: '24', 
+    break_after: true,
+           type: 'text-field-small'},
+  {       text: '<div><p><h2>Part 4 Watching the test video</h2><br>After watching the whole video, please click continue to enter the last part. Please note you are requested to finish watching the entire video.</p><p><iframe class="video" src="http://youtube.com/embed/R02hkumR8gw" style="width: 760px; height: 573px;"></iframe></p><p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p></div>', 
+             id: '25', 
+    break_after: true,
+           type: 'text-field-large'},
+ {        text: '<div><p><h2>Part 5 Video Experience Questionnaire</h2><br>In this part, you will be asked to fill in several questions related to your visual experience. Note that the experience we asked is ONLY refer to the video you just watched. Please answer all questions honestly.</p><p><b>The video clip was enjoyable</b></p></div>', 
+             id: '26', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["fully disagree", "fully agree"]},
+  {        text: '<p><b>I would recommend this video clip to my friends</b></p>', 
+             id: '27', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["fully disagree", "fully agree"]},
+  {        text: '<p><b>I was satisfied with the experience in watching this video clip</b></p>', 
+             id: '28', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["fully disagree", "fully agree"]},
+  {        text: '<p><b>When I was watching the video, I still paid attention to the world around me</b></p>', 
+             id: '29', 
+           type: 'single-select-oneline', 
+        options: ["1", "2", "3", "4", "5", "6", "7"],
+         labels: ["fully disagree", "fully agree"]},
+  {        text: '<p><b>Did you perceive any visual impairments in the video?</b></p>', 
+             id: '30', 
            type: 'single-select', 
-        options: ["a stamp", "coffee", "a dog", "plants"]}
+        options: ["Very annoying", "Annoying", "Slightly annoying", "Perceptible, but not annoying", "Imperceptiable"]},
+  {        text: '<p><b>Which type of sport they played in the video?</b></p>', 
+             id: '31', 
+       required: true,
+           type: 'text-field-small'},
+  {        text: '<p><b>General feedback</b><br>If you encounter any problems with this task or have any other comments, please report them here. You can also use this textbox to tell us if you liked this Task or have any suggestions.</p>', 
+             id: '32', 
+    break_after: true,
+           type: 'text-field-large'}
 ];
 
 var firstQuestionDisplayed = -1;
@@ -65,20 +168,26 @@ $(document).ready(function(){
   });
   
   $('#nextBtn').click(function() {
-    if ( $('#nextBtn').text().indexOf('Continue') === 0 ) {
-      var ok = true;
-      for (i = firstQuestionDisplayed; i <= lastQuestionDisplayed; i++) {
-        if (questions[i]['required'] === true && !getQuestionAnswer(questions[i])) {
-          $('.question-container > div.question:nth-child(' + (i+1) + ') > .required-message').show();
-          ok = false;
-        }
+    var ok = true;
+    for (i = firstQuestionDisplayed; i <= lastQuestionDisplayed; i++) {
+      if (questions[i]['required'] === true && !getQuestionAnswer(questions[i])) {
+        $('.question-container > div.question:nth-child(' + (i+1) + ') > .required-message').show();
+        ok = false;
       }
-      if (ok)
-        showNextQuestionSet();
-    } else {
-      // call submitting stuff here.  
     }
-    
+    if (!ok)
+      return
+
+    if ( $('#nextBtn').text().indexOf('Continue') === 0 ) {
+        showNextQuestionSet();
+    }
+    else {
+      var answers = {};
+      for (i = 0; i < questions.length; i++) {
+        answers["q_" + questions[i].id] = getQuestionAnswer(questions[i]);
+      }
+      alert(JSON.stringify(answers));
+    }
   });
   
   showNextQuestionSet();
@@ -92,13 +201,16 @@ function getQuestionAnswer(question) {
   if ( question.type === 'single-select' ) {
     result = $('input[type="radio"][name="' + questionId + '"]:checked').val();
   }
+  else if ( question.type === 'single-select-oneline' ) {
+    result = $('input[type="radio"][name="' + questionId + '"]:checked').val();
+  }
   else if ( question.type === 'text-field-small' ) {
     result = $('input[name=' + questionId + ']').val();
   }
   else if ( question.type === 'text-field-large' ) {
     result = $('textarea[name=' + questionId + ']').val();
   }
-  return result;
+  return result ? result : undefined;
 }
 
 
@@ -121,6 +233,19 @@ function generateQuestionElement(question) {
       questionAnswerElement.append(
         '<label class="radio"><input type="radio" value="' + option + '" name="' + questionId + '"/>' + option + '</label>');
     });
+  }
+  else if ( question.type === 'single-select-oneline' ) {
+    questionElement.addClass('single-select-oneline');
+    var html = '<table border="0" cellpadding="5" cellspacing="0"><tr><td></td>';
+    question.options.forEach(function(label) {
+      html += '<td><label>' + label + '</label></td>';
+    });
+    html += '<td></td></tr><tr><td><div>' + question.labels[0] + '</div></td>';
+    question.options.forEach(function(label) {
+      html += '<td><div><input type="radio" value="' + label + '" name="' + questionId + '"></div></td>';
+    });
+    html += '<td><div>' + question.labels[1] + '</div></td></tr></table>';
+    questionAnswerElement.append(html);
   }
   else if ( question.type === 'text-field-small' ) {
     questionElement.addClass('text-field-small');
